afbe647d77486ab73c035fb87c16b500cef92103
muppireddyharish <167728482+muppireddyharish@users.noreply.github.com>
2024-04-22 12:12:42 -0400

    Create config file for snowflake

---
+++ snowflake/snowflake_config.py
@@ -0,0 +1,12 @@
+snowflake_config = {
+    'user': 'mharishr',
+    'password': '****',
+    'account': 'lwb54221.us-east-1',
+    'region': 'us-east-1',
+    'warehouse': 'COMPUTE_WH',
+    'database': 'STUDENTS_STAGING',
+    'schema': 'STUDENTS_STG',
+    'table_name': 'students_merged_db',
+    'students_file_path': '/Users/datapro/Documents/pyproject/students.json',
+    'missed_days_file_path': '/Users/datapro/Documents/pyproject/missed_days.json'
+}
